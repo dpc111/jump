b164fee42971b6a984e4d4bb6bc852681ce29e06
dpc111 <1539309743@qq.com>
2018-04-02 18:48:56 +0800

    l

--- jump/Assets/scripts/mgr/StakeMgr.cs
+++ jump/Assets/scripts/mgr/StakeMgr.cs
@@ -6,6 +6,7 @@ public class StakeMgr : MonoBehaviour {
     public static Dictionary<int, GameObject> StakePrefabs = new Dictionary<int, GameObject>();
     public static Dictionary<int, int> StakeLens = new Dictionary<int, int>(); 
     public static List<GameObject> Stakes = new List<GameObject>();
+    public static int LastIndex = 1;
     public static int CurMinIndex = 1;
     public static int CurMaxIndex = 1;
 
@@ -19,15 +20,44 @@ public class StakeMgr : MonoBehaviour {
 		
 	}
 
-    void AddPlan()
+    public static void AddPlan()
     {
         int size = StakePrefabs.Count;
         int rindex = Random.Range(1, size);
         GameObject rObject = StakePrefabs[rindex];
+
         size = StakeLens.Count;
         rindex = Random.Range(1, size);
-        int lastStakeIndex = Stakes.Count;
-        GameObject lastStake = Stakes[lastStakeIndex];
-        Stack stack = lastStake.GetComponent<Stack>();
+        int len = StakeLens[rindex];
+
+        GameObject lastStake = Stakes[Stakes.Count];
+        StakePro stakePro = lastStake.GetComponent<StakePro>();
+        LastIndex = stakePro.Index + 1;
+        Vector3 pos = stakePro.Cen + new Vector3(0, len + stakePro.Len, 0);
+        Quaternion rot = Quaternion.Euler(0f, 0f, 0f);
+
+        GameObject newStake = Instantiate(rObject, pos, rot);
+        StakePro newStakePro = newStake.GetComponent<StakePro>();
+        newStakePro.Index = LastIndex;
+        newStakePro.Len = len;
+        newStakePro.Cen = pos;
+
+        Stakes.Add(newStake);
+    }
+
+    public static void ReachStack(int index)
+    {
+        CurMaxIndex = index;
+        CurMinIndex = index;
+        while (Stakes.Count < CurMaxIndex + 3)
+        {
+            AddPlan();
+        }
+
+        while (Stakes.Count > 0)
+        {
+            GameObject stake = Stakes[1];
+            StakePro stakePro = stake.GetComponent<StakePro>();
+        }
     }
 }
--- jump/Assets/scripts/stake/StakePro.cs
+++ jump/Assets/scripts/stake/StakePro.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stake : MonoBehaviour {
-    public float Lenth = 0f;
+public class StakePro : MonoBehaviour {
+    public int Index = 0;
+    public float Len = 0f;
     public Vector3 Cen;
 	// Use this for initialization
 	void Start () {
